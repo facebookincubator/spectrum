2062f1ebb1698bf4af4dd8bbcdab2d8977fdc3f6
Daniel Hugenroth <hugenroth@fb.com>
2019-05-13 14:24:09 -0700

    Add ComparisionUtil with Builder pattern for end-to-end tests
    Summary:
    @public
    
    This change adds a testing util that allows to write declarative end-to-end tests for common Spectrum test cases.
    
    Reviewed By: oprisnik
    
    Differential Revision: D15198580
    
    fbshipit-source-id: d8a7bde0a8841d018a0a27e6b7c7eee0086355f7

---
+++ android/spectrumtestutils/src/androidTest/java/com/facebook/spectrum/testutils/SpectrumAssertUtilsTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.spectrum.testutils;
+
+import android.graphics.Bitmap;
+import com.facebook.spectrum.image.EncodedImageFormat;
+import com.facebook.spectrum.options.DecodeOptions;
+import com.facebook.spectrum.options.EncodeOptions;
+import com.facebook.spectrum.requirements.EncodeRequirement;
+import com.facebook.spectrum.testutils.SpectrumAssertUtils.Builder;
+import java.io.IOException;
+import org.fest.assertions.data.Offset;
+import org.junit.Test;
+
+public class SpectrumAssertUtilsTest {
+
+  @Test
+  public void testBuilder_whenCreating_thenNothingThrows() throws IOException {
+    final EncodeOptions options =
+        EncodeOptions.Builder(new EncodeRequirement(EncodedImageFormat.JPEG, 50)).build();
+
+    final Builder builder =
+        Builder.withTestImage(TestData.SAMPLE_85x128_q15)
+            .encoding(options)
+            .comparingAgainstTestFile(TestData.SAMPLE_85x128_q15)
+            .assertingOutputFormat(EncodedImageFormat.JPEG)
+            .usingSsimTarget(1.0f)
+            .usingSsimTolerance(Offset.offset(0.05f));
+
+    builder.validate();
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testBuilder_whenRequestingBadConfig_thenValidateThrows() {
+    final DecodeOptions options = DecodeOptions.Builder().build();
+
+    final Builder builder =
+        Builder.withTestImage(TestData.SAMPLE_85x128_q15)
+            .decoding(options)
+            .comparingAgainst(Bitmap.createBitmap(10, 10, Bitmap.Config.ARGB_8888))
+            .assertingOutputExifOrientation(2);
+
+    builder.validate();
+  }
+}
