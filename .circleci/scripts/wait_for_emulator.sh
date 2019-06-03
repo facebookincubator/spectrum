9b627f002755dcdc26953d8c91fbbde58317df38
Daniel Hugenroth <hugenroth@fb.com>
2019-06-03 11:48:39 +0100

    update

--- .circleci/scripts/wait_for_emulator.sh
+++ .circleci/scripts/wait_for_emulator.sh
@@ -3,9 +3,11 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+#
+# Extracted from https://github.com/facebook/react-native/blob/master/scripts/android-setup.sh
 
 echo "Waiting for Android Virtual Device to finish booting..."
-local bootanim=""
+bootanim=""
 export PATH=$(dirname $(dirname $(command -v android)))/platform-tools:$PATH
 until [[ "$bootanim" =~ "stopped" ]]; do
   sleep 5
