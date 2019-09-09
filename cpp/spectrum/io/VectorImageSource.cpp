3b747351b68622d26b02d05735292a9016652d21
Zach Zagorski <zzagorsk@fb.com>
2019-09-09 11:09:03 -0700

    Rewrite VectorImageSource using templates
    Summary: VectorImageSink already uses templates. Let's make them consistent.
    
    Reviewed By: cuva
    
    Differential Revision: D17121413
    
    fbshipit-source-id: 14a39aa98f74e94113ed9a7fbfa11044cfdd02a5

--- cpp/spectrum/io/VectorImageSource.cpp
+++ cpp/spectrum/io/VectorImageSource.cpp
@@ -12,24 +12,22 @@
 #include <algorithm>
 #include <cstddef>
 #include <stdexcept>
-#include <vector>
 
 namespace facebook {
 namespace spectrum {
 namespace io {
 
-//
-// VectorImageSourceHandler
-//
+template <class Interface>
+VectorImageSource<Interface>::VectorImageSource(std::vector<char> data)
+    : Interface(), _data(std::move(data)) {}
 
-VectorImageSourceHandler::VectorImageSourceHandler(std::vector<char> data)
-    : _data(std::move(data)) {}
-
-std::size_t VectorImageSourceHandler::_getTotalBytesRead() const {
+template <class Interface>
+std::size_t VectorImageSource<Interface>::getTotalBytesRead() const {
   return _offset;
 }
 
-std::size_t VectorImageSourceHandler::_read(
+template <class Interface>
+std::size_t VectorImageSource<Interface>::read(
     char* const destination,
     const std::size_t length) {
   const auto bytesLeft = _data.size() - _offset;
@@ -41,59 +39,13 @@ std::size_t VectorImageSourceHandler::_read(
   return bytesToRead;
 }
 
-std::size_t VectorImageSourceHandler::_available() {
+template <class Interface>
+std::size_t VectorImageSource<Interface>::available() {
   return _data.size() - _offset;
 }
 
-//
-// VectorBitmapImageSource
-//
-
-VectorBitmapImageSource::VectorBitmapImageSource(
-    std::vector<char> data,
-    const image::Specification& imageSpecification)
-    : IBitmapImageSource(),
-      _delegate(std::move(data)),
-      _imageSpecification(imageSpecification) {}
-
-image::Specification VectorBitmapImageSource::imageSpecification() const {
-  return _imageSpecification;
-}
-
-std::size_t VectorBitmapImageSource::read(
-    char* const destination,
-    const std::size_t length) {
-  return _delegate._read(destination, length);
-}
-
-std::size_t VectorBitmapImageSource::getTotalBytesRead() const {
-  return _delegate._getTotalBytesRead();
-}
-
-std::size_t VectorBitmapImageSource::available() {
-  return _delegate._available();
-}
-
-//
-// VectorEncodedImageSource
-//
-
-VectorEncodedImageSource::VectorEncodedImageSource(std::vector<char> data)
-    : IEncodedImageSource(), _delegate(std::move(data)) {}
-
-std::size_t VectorEncodedImageSource::read(
-    char* const destination,
-    const std::size_t length) {
-  return _delegate._read(destination, length);
-}
-
-std::size_t VectorEncodedImageSource::getTotalBytesRead() const {
-  return _delegate._getTotalBytesRead();
-}
-
-std::size_t VectorEncodedImageSource::available() {
-  return _delegate._available();
-}
+template class VectorImageSource<IBitmapImageSource>;
+template class VectorImageSource<IEncodedImageSource>;
 
 } // namespace io
 } // namespace spectrum
--- cpp/spectrum/io/VectorImageSource.h
+++ cpp/spectrum/io/VectorImageSource.h
@@ -16,22 +16,30 @@ namespace facebook {
 namespace spectrum {
 namespace io {
 
-class VectorBitmapImageSource;
-class VectorEncodedImageSource;
+template <class Interface>
+class VectorImageSource : public Interface {
+ public:
+  explicit VectorImageSource(std::vector<char> data);
 
-class VectorImageSourceHandler {
-  friend class VectorBitmapImageSource;
-  friend class VectorEncodedImageSource;
+  VectorImageSource(const VectorImageSource&) = delete;
+  VectorImageSource(VectorImageSource&&) = default;
+  ~VectorImageSource() override = default;
+
+  std::size_t read(char* const destination, const std::size_t length) override;
+  std::size_t getTotalBytesRead() const override;
+  std::size_t available() override;
 
  private:
-  explicit VectorImageSourceHandler(std::vector<char> data);
-  std::size_t _read(char* const destination, const std::size_t length);
-  std::size_t _getTotalBytesRead() const;
-  std::size_t _available();
-
   const std::vector<char> _data;
   std::size_t _offset = 0;
 };
+
+/**
+ * The vector encoded image source wraps encoded image data.
+ *
+ * This is the prefered way to provide encoded image data in C++ call sites.
+ */
+using VectorEncodedImageSource = VectorImageSource<IEncodedImageSource>;
 
 /**
  * The vector bitmap image source wraps decoded pixel data together with its
@@ -39,57 +47,19 @@ class VectorImageSourceHandler {
  *
  * This is the prefered way to provide decoded image data in C++ call sites.
  */
-class VectorBitmapImageSource : public IBitmapImageSource {
+class VectorBitmapImageSource : public VectorImageSource<IBitmapImageSource> {
  public:
-  /**
-   * New vector bitmap image source with the given pixel data and specification.
-   * The match of the vector and image specification is not enforced during
-   * construction.
-   *
-   * @param data vector of pixel data that will be consumed by `read` calls.
-   * @param imageSpecification image specification that matches the provided
-   * pixel data.
-   */
   explicit VectorBitmapImageSource(
       std::vector<char> data,
-      const image::Specification& imageSpecification);
-
-  VectorBitmapImageSource(const VectorBitmapImageSource&) = delete;
-  VectorBitmapImageSource(VectorBitmapImageSource&&) = default;
-  ~VectorBitmapImageSource() override = default;
-
-  std::size_t read(char* const destination, const std::size_t length) override;
-  std::size_t getTotalBytesRead() const override;
-  std::size_t available() override;
-  image::Specification imageSpecification() const override;
+      const image::Specification& imageSpecification)
+      : VectorImageSource<IBitmapImageSource>(data),
+        _imageSpecification(imageSpecification) {}
+  image::Specification imageSpecification() const override {
+    return _imageSpecification;
+  }
 
  private:
-  VectorImageSourceHandler _delegate;
   image::Specification _imageSpecification;
-};
-
-/**
- * The vector bitmap image source wraps encoded image data.
- *
- * This is the prefered way to provide encoded image data in C++ call sites.
- */
-class VectorEncodedImageSource : public IEncodedImageSource {
- public:
-  /**
-   * @param data vector with the encoded image bytes
-   */
-  explicit VectorEncodedImageSource(std::vector<char> data);
-
-  VectorEncodedImageSource(const VectorEncodedImageSource&) = delete;
-  VectorEncodedImageSource(VectorEncodedImageSource&&) = default;
-  ~VectorEncodedImageSource() override = default;
-
-  std::size_t read(char* const destination, const std::size_t length) override;
-  std::size_t getTotalBytesRead() const override;
-  std::size_t available() override;
-
- private:
-  VectorImageSourceHandler _delegate;
 };
 
 } // namespace io
